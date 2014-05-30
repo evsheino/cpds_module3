84d6efdf6488d9f99f70bec105aad19291ae5ba0
Erkki Heino <evsheino@gmail.com>
2014-05-30 12:02:39 +0200

    read/write ratio

--- opty/opty.erl
+++ opty/opty.erl
@@ -1,13 +1,13 @@
 -module(opty).
--export([start/4, stop/1]).
+-export([start/6, stop/1]).
 
 %% Clients: Number of concurrent clients in the system
 %% Entries: Number of entries in the store
 %% Updates: Number of write operations per transaction
 %% Time: Duration of the experiment (in secs)
-start(Clients, Entries, Updates, Time) ->
+start(Clients, Entries, Updates, Time, Reads, Writes) ->
     register(s, server:start(Entries)),
-    L = startClients(Clients, [], Entries, Updates),
+    L = startClients(Clients, [], Entries, Updates, Reads, Writes),
     io:format("Starting: ~w CLIENTS, ~w ENTRIES, ~w UPDATES PER TRANSACTION, 
     DURATION ~w s ~n", [Clients, Entries, Updates, Time]),
     timer:sleep(Time*1000),
@@ -18,10 +18,10 @@ stop(L) ->
     stopClients(L),
     s ! stop.
 
-startClients(0,L,_,_) -> L;
-startClients(Clients, L, Entries, Updates) ->
-    Pid = client:start(Clients, Entries, Updates, s),
-    startClients(Clients-1, [Pid|L], Entries, Updates).
+startClients(0,L,_,_,_,_) -> L;
+startClients(Clients, L, Entries, Updates, Reads, Writes) ->
+    Pid = client:start(Clients, Entries, Updates, s, Reads, Writes),
+    startClients(Clients-1, [Pid|L], Entries, Updates, Reads, Writes).
 
 stopClients([]) -> ok;
 stopClients([Pid|L]) ->
