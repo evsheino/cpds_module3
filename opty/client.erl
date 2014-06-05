81915cdedfbfb5deee56946cb3baa8627f131c65
Bastien S <bastien42@gmail.com>
2014-06-05 14:42:35 +0200

    subset of entries working

--- opty/client.erl
+++ opty/client.erl
@@ -1,15 +1,16 @@
 -module(client).
--export([start/6]).
+-export([start/7]).
 
-start(Name, Entries, Updates, Server, Reads, Writes) ->
-    spawn(fun() -> open(Name, Entries, Updates, Server, 0, 0, Reads, Writes) end).
+start(Name, Entries, Updates, Server, Reads, Writes, Subset) ->
+    spawn(fun() -> open(Name, Entries, Updates, Server, 0, 0, Reads, Writes, Subset) end).
 
-open(Name, Entries, Updates, Server, Total, Ok, Reads, Writes) ->
+open(Name, Entries, Updates, Server, Total, Ok, Reads, Writes, Subset) ->
     {A1,A2,A3} = now(),
     random:seed(A1, A2, A3),
     Server ! {open, self()},
     receive
         {stop, From} ->
+            io:format("~w: Subset used:~w ~n",[Name, Subset]),
             io:format("~w: Transactions TOTAL:~w, OK:~w, -> ~w % ~n",
             [Name, Total, Ok, 100*Ok/Total]),
             From ! {done, self()},
@@ -17,11 +18,11 @@ open(Name, Entries, Updates, Server, Total, Ok, Reads, Writes) ->
         {transaction, Validator, Store} ->
             Handler = handler:start(self(), Validator, Store),
             do_transactions(Name, Entries, Updates, Server, Handler,
-                            Total, Ok, Updates, Reads, Writes)
+                            Total, Ok, Updates, Reads, Writes, Subset)
     end.
 
 % Commit transaction
-do_transactions(Name, Entries, Updates, Server, Handler, Total, Ok, 0, Reads, Writes) ->
+do_transactions(Name, Entries, Updates, Server, Handler, Total, Ok, 0, Reads, Writes, Subset) ->
     %io:format("~w: Commit: TOTAL ~w, OK ~w~n", [Name, Total, Ok]),
     %timer:sleep(Name*10),
     Ref = make_ref(),
@@ -29,18 +30,18 @@ do_transactions(Name, Entries, Updates, Server, Handler, Total, Ok, 0, Reads, Wr
     Result = receiveCommitValue(Ref),
     if
         Result == ok ->
-            open(Name, Entries, Updates, Server, Total+1, Ok+1, Reads, Writes);
+            open(Name, Entries, Updates, Server, Total+1, Ok+1, Reads, Writes, Subset);
         true ->
-            open(Name, Entries, Updates, Server, Total+1, Ok, Reads, Writes)
+            open(Name, Entries, Updates, Server, Total+1, Ok, Reads, Writes, Subset)
     end;
 
 % Reads and Writes
-do_transactions(Name, Entries, Updates, Server, Handler, Total, Ok, N, Reads, Writes) ->
+do_transactions(Name, Entries, Updates, Server, Handler, Total, Ok, N, Reads, Writes, Subset) ->
     %io:format("~w: R/W: TOTAL ~w, OK ~w, N ~w~n", [Name, Total, Ok, N]),
     Ref = make_ref(),
-    Num = random:uniform(Entries),
+    Index = random:uniform(tuple_size(Subset)),
+    Num = element(Index, Subset),
     do_reads(Handler, Ref, Num, Reads),
-%    Handler ! {read, Ref, Num},
     if
 	Reads == 0 ->
 	    Value = 1;
@@ -48,8 +49,7 @@ do_transactions(Name, Entries, Updates, Server, Handler, Total, Ok, N, Reads, Wr
 	    Value = receiveValue(Ref)
     end,
     do_writes(Handler, Num, Value, Writes),
-%    Handler ! {write, Num, Value+1},
-    do_transactions(Name, Entries, Updates, Server, Handler, Total, Ok, N-1, Reads, Writes).
+    do_transactions(Name, Entries, Updates, Server, Handler, Total, Ok, N-1, Reads, Writes, Subset).
 
 do_reads(_, _, _, 0) ->
     ok;
--- opty/opty.erl
+++ opty/opty.erl
@@ -1,27 +1,42 @@
 -module(opty).
--export([start/6, stop/1]).
+-export([start/7, stop/1]).
 
 %% Clients: Number of concurrent clients in the system
 %% Entries: Number of entries in the store
 %% Updates: Number of write operations per transaction
 %% Time: Duration of the experiment (in secs)
-start(Clients, Entries, Updates, Time, Reads, Writes) ->
+start(Clients, Entries, Updates, Time, Reads, Writes, Num_Subset) ->
     register(s, server:start(Entries)),
-    L = startClients(Clients, [], Entries, Updates, Reads, Writes),
+    L = startClients(Clients, [], Entries, Updates, Reads, Writes, Num_Subset),
     io:format("Starting: ~w CLIENTS, ~w ENTRIES, ~w UPDATES PER TRANSACTION, 
     DURATION ~w s ~n", [Clients, Entries, Updates, Time]),
     timer:sleep(Time*1000),
     stop(L).
+
+
+generate_subset(0, _, Subset) ->
+    list_to_tuple(Subset);
+
+generate_subset(Number, Entries, Subset) ->
+    Temp = random:uniform(Entries),
+    case lists:any(fun(E) -> E == Temp end, Subset) of
+	true ->
+	    generate_subset(Number, Entries, Subset);
+	false ->
+	    generate_subset(Number - 1, Entries, [Temp|Subset])
+    end.
+
 
 stop(L) ->
     io:format("Stopping...~n"),
     stopClients(L),
     s ! stop.
 
-startClients(0,L,_,_,_,_) -> L;
-startClients(Clients, L, Entries, Updates, Reads, Writes) ->
-    Pid = client:start(Clients, Entries, Updates, s, Reads, Writes),
-    startClients(Clients-1, [Pid|L], Entries, Updates, Reads, Writes).
+startClients(0,L,_,_,_,_,_) -> L;
+startClients(Clients, L, Entries, Updates, Reads, Writes, Num_Subset) ->
+    Subset = generate_subset(Num_Subset, Entries, []),
+    Pid = client:start(Clients, Entries, Updates, s, Reads, Writes, Subset),
+    startClients(Clients-1, [Pid|L], Entries, Updates, Reads, Writes, Num_Subset).
 
 stopClients([]) -> ok;
 stopClients([Pid|L]) ->
